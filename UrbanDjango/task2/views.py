2680a2d27f5186e87a11b6f99637203ec75540eb
Katkov1969 <126957788+Katkov1969@users.noreply.github.com>
2024-10-16 20:11:34 +0300

    Add files via upload

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def class_page(request):
-    return render(request, 'class_template.html')
+    return render(request, 'second_task/class_template.html')
 
 def func_page(request):
-    return render(request, 'func_template.html')
+    return render(request, 'second_task/func_template.html')
 
