5365dc11695801911454c2630c719994a9f7fcdd
Konstantin1969 <kkatkoff69@gmail.com>
2024-10-16 14:23:23 +0300

    second

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import UrbanDjango.task2
+
+from task2.views import class_page, func_page
+#from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('class/', class_page),
+    path('func/', func_page),
 ]
